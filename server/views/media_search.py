e8e5a6131d2dfaca133bea8e75915b71397fbc70
cindyb <cindyloo_home at yahoo.com>
2020-06-16 11:06:55 -0400

    pylint edits

--- server/views/media_search.py
+++ server/views/media_search.py
@@ -12,7 +12,7 @@ def media_search_with_page(search_str, tags_id=None, **kwargs):
     link_id = request.args.get('linkId') if 'linkId' in request.args else 0
     user_mc = user_admin_mediacloud_client()
     media_page = user_mc.mediaList(name_like=search_str, tags_id=tags_id, last_media_id=link_id, rows=100, sort="num_stories", **kwargs)
-    if len(media_page) ==0:
+    if len(media_page) == 0:
         last_media_id = -1
     else:
         last_media_id = media_page[len(media_page)-1]['media_id']
@@ -26,14 +26,13 @@ def collection_search_with_page(search_str, public_only, tag_sets_id_list, **kwa
     link_id = request.args.get('linkId') if 'linkId' in request.args else 0
     user_mc = user_mediacloud_client()
 
-    collection_page = user_mc.tagList(tag_sets_id_list, public_only=public_only, name_like=search_str, last_tags_id=link_id,)
-    if len(collection_page) ==0:
+    collection_page = user_mc.tagList(tag_sets_id_list, public_only=public_only, name_like=search_str, rows=100, last_tags_id=link_id,)
+    if len(collection_page) == 0:
         last_tags_id = -1
     else:
         last_tags_id = collection_page[len(collection_page)-1]['tags_id']
     return collection_page, last_tags_id
 
 def collection_search(search_str, public_only, tag_sets_id_list):
-
     user_mc = user_mediacloud_client()
     return user_mc.tagList(tag_sets_id_list, public_only=public_only, name_like=search_str)
