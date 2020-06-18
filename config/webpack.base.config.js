6b166fc8cf50c86d6db5857f91b34743c92b3804
Dennis Jen <dsjen@media.mit.edu>
2020-06-18 16:10:42 -0400

    Use "version" from package.json

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -22,6 +22,7 @@ const baseConfig = {
     new MomentLocalesPlugin(),
     // provide much cleaner feedback while building on the command line
     new WebpackBar(),
+    new webpack.DefinePlugin({ MC_VERSION: JSON.stringify(require('../package.json').version) }),
   ],
   stats: 'minimal',
   module: {
