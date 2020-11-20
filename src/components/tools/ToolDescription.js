3643eee838042de1b09086a348024ca3a773aae9
Obed Amoasi <obedamoasi@gmail.com>
2020-11-20 14:17:29 +0000

    redesign tools home

--- src/components/tools/ToolDescription.js
+++ src/components/tools/ToolDescription.js
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { FormattedMessage, injectIntl } from 'react-intl';
+import AppButton from '../.../../common/AppButton';
 
 const ToolDescription = (props) => {
-  const { name, description, screenshotUrl, url, className } = props;
+  const { name, description,buttonLabel, screenshotUrl, url, className } = props;
   const { formatMessage } = props.intl;
   return (
     <a href={url}>
       <div className={`tool-description ${className}`}>
         <h2><FormattedMessage {...name} /></h2>
         <p><FormattedMessage {...description} /></p>
-        <img src={screenshotUrl} alt={formatMessage(name)} width="100%" />
+        <AppButton className="white" label="Call to Action">
+      <FormattedMessage {...buttonLabel} />
+    </AppButton>
       </div>
     </a>
   );
--- src/components/tools/ToolsHomeContainer.js
+++ src/components/tools/ToolsHomeContainer.js
@@ -12,10 +12,13 @@ import LoginForm from '../user/LoginForm';
 import DataCard from '../common/DataCard';
 import { assetUrl } from '../../lib/assetUtil';
 import PageTitle from '../common/PageTitle';
+import AppButton from '../.../../common/AppButton';
 
 const localMessages = {
-  title: { id: 'tools.home.title', defaultMessage: 'Welcome to Media Cloud' },
-  intro: { id: 'tools.home.intro', defaultMessage: 'Understanding attention and influence within media ecosystems.' },
+  title: { id: 'tools.home.title', defaultMessage: 'What is Media Cloud' },
+  intro: { id: 'tools.home.intro', defaultMessage: 'Mediacloud is an open-source content analysis tool that automates the collection and classification of online news content and other data sources in African media ecosystems' },
+  civicTitle: { id: 'tools.civic.title', defaultMessage: 'What is CivicSignal?' },
+  civicIntro: { id: 'tools.civic.intro', defaultMessage: 'CivicSignal produces research and analyses of Africa’s media ecosystem and emerging civic technology sector, addressing issues that impact democracies and  civil society.' },
   loginTitle: { id: 'tools.home.login.title', defaultMessage: 'Login or Signup Now' },
 };
 
@@ -23,15 +26,10 @@ const ToolsHomeContainer = (props) => {
   const { isLoggedIn } = props;
   const notLoggedInContent = (
     <Row>
-      <Col lg={8}>
+      <Col lg={12}>
         <Faq />
       </Col>
-      <Col lg={4}>
-        <DataCard>
-          <h2><FormattedMessage {...localMessages.loginTitle} /></h2>
-          <LoginForm redirect="/" />
-        </DataCard>
-      </Col>
+   
     </Row>
   );
   const loggedInContent = (
@@ -47,41 +45,67 @@ const ToolsHomeContainer = (props) => {
       <PageTitle />
       <Grid>
         <Row>
-          <Col lg={12}>
+        <Col lg={6}>
+            <h1><FormattedMessage {...localMessages.civicTitle} /></h1>
+            <p className="intro"><FormattedMessage {...localMessages.civicIntro} /></p>
+            <div className="cta-container">
+            <AppButton className="contained-btn" variant="contained" label="Call to Action">
+              <FormattedMessage {...messages.learnMoreButton} />
+            </AppButton>
+            </div>
+          </Col>
+          <Col lg={6}>
             <h1><FormattedMessage {...localMessages.title} /></h1>
             <p className="intro"><FormattedMessage {...localMessages.intro} /></p>
+            <div className="cta-container">
+            <AppButton label="Call to Action">
+              <FormattedMessage {...messages.learnMoreButton} />
+            </AppButton>
+            </div>
           </Col>
         </Row>
         <Row>
-          <Col lg={4}>
+          <Col lg={6}>
             <ToolDescription
               name={messages.explorerToolName}
-              className="tool-explorer"
+              className="tool-explorer tools-card"
               description={messages.explorerToolDescription}
               screenshotUrl={assetUrl('/static/img/preview-explorer.png')}
               url={urlToExplorer('home')}
+              buttonLabel={messages.learnMoreButton}
             />
           </Col>
-          <Col lg={4}>
+          <Col lg={6}>
             <ToolDescription
               name={messages.topicsToolName}
-              className="tool-topics"
+              className="tool-topics tools-card"
               description={messages.topicsToolDescription}
               screenshotUrl={assetUrl('/static/img/preview-topics.png')}
               url={urlToTopicMapper('home')}
+              buttonLabel={messages.learnMoreButton}
             />
           </Col>
-          <Col lg={4}>
+          <Col lg={6}>
             <ToolDescription
               name={messages.sourcesToolName}
-              className="tool-sources"
+              className="tool-sources tools-card"
               description={messages.sourcesToolDescription}
               screenshotUrl={assetUrl('/static/img/preview-sources.png')}
               url={urlToSourceManager('home')}
+              buttonLabel={messages.learnMoreButton}
+            />
+          </Col>
+          <Col lg={6}>
+            <ToolDescription
+              name={messages.mediaDataToolName}
+              className="media-data tools-card"
+              description={messages.mediaDataToolDescription}
+              screenshotUrl={assetUrl('/static/img/preview-sources.png')}
+              url={urlToSourceManager('home')}
+              buttonLabel={messages.learnMoreButton}
             />
           </Col>
         </Row>
-        <SystemStatsContainer />
         { content }
       </Grid>
     </div>
@@ -98,8 +122,8 @@ const mapStateToProps = state => ({
 });
 
 export default
-injectIntl(
-  connect(mapStateToProps)(
-    ToolsHomeContainer
-  )
-);
+  injectIntl(
+    connect(mapStateToProps)(
+      ToolsHomeContainer
+    )
+  );
